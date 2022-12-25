1d5dadf5f655b186d4335ee1ec0ee816ffcec2e3
Ramana Reddy <ramanareddy.manda@practo.com>
2022-12-25 14:16:05 +0530

    resolve merge conflicts

---
+++ src/main/java/com/example/studentgradingdemo/StudentGradingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentgradingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentGradingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentGradingDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studentgradingdemo/StudentGradingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentgradingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentGradingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
